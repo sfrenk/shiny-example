eb150073243b2aa0b6862f63587e9e390fd7e0c4
sfrenk <stephen.frenk@gmail.com>
2017-04-14 19:34:02 -0400

    made quantile selection smoother

--- ui.R
+++ ui.R
@@ -25,13 +25,13 @@ shinyUI(pageWithSidebar(
         # Quantile selection to caluclate alpha and beta
         strong("Calculate alpha and beta based on two quantiles"),
         fluidRow(
-            column(6, numericInput("q1", "Quantile 1:", 0.25)),
-            column(6, numericInput("p1", "Prob 1:", 0.25))
+            column(6, numericInput("q1", "Quantile 1:", 0.25, min = 0, max = 1, step = 0.01)),
+            column(6, numericInput("p1", "Prob 1:", 0.25, min = 0, max = 1, step = 0.01))
         ),
         
         fluidRow(   
-            column(6, numericInput("q2", "Quantile 2:", 0.75)),
-            column(6, numericInput("p2", "Prob 2:", 0.75))
+            column(6, numericInput("q2", "Quantile 2:", 0.75, min = 0, max = 1, step = 0.01)),
+            column(6, numericInput("p2", "Prob 2:", 0.75, min = 0, max = 1, step = 0.01))
         ),
         
         actionButton("go", "Go!")
